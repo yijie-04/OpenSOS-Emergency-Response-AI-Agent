42ed00414a3391ea6a7e2854f435d819d69d693e
RachelYangZY <ryangzy@gmail.com>
2023-11-04 16:38:02 -0400

    new

--- InteractionWithCaller.py
+++ InteractionWithCaller.py
@@ -1,11 +1,19 @@
 import AudioToText
 import TextToAudio
+import API_1
 
 ## Call is received
 
-# initial prompt
-TextToAudio.translate("9 1 1, what's your emergency?")
 
-# receive audio input
-input = AudioToText.translate()
-print(input)
+keep_talking = 1
+# initial prompt
+say = "9 1 1, what's your emergency?"
+while keep_talking == 1:
+    TextToAudio.translate(say)
+
+    # receive audio input
+    input = AudioToText.translate()
+    print(input)
+
+    API_1.get_completion(input)
+
