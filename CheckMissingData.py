c433c2a8996724b393eeecab7426c86570ef9a7a
RachelYangZY <ryangzy@gmail.com>
2023-11-04 18:29:39 -0400

    commit plz

--- CheckMissingData.py
+++ CheckMissingData.py
@@ -1,4 +1,16 @@
-def check():
+from info import Caller
+
+def check(cur_caller):
     # do a for loop going through each required information. 
     # If None, have a prompt audibly ask for the information
-    print("No")
+    if cur_caller.caller_name == None:
+        return "What is your name?"
+    elif cur_caller.incident_location == None:
+        return "Where are you?"
+    elif cur_caller.symptoms == None:
+        return "What are the symptoms?"
+    else:
+        return None
+    
+    
+    
