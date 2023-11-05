9c69ff0f60bf528c4e0e6e361f3f8fdd916c751e
Yijie Wang <yijiewang0806@gmail.com>
2023-11-04 20:26:41 -0400

    commit with ai interface

--- API_1.py
+++ API_1.py
@@ -1,62 +1,118 @@
 import openai
 import os
-from info import caller
+from info import Caller
 #from dotenv import load_dotenv, find_dotenv
 
-_ = load_dotenv(find_dotenv())
-os.environ["OPENAI_API_KEY"] = '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'
+#_ = load_dotenv(find_dotenv())
+sym = None
+os.environ["OPENAI_API_KEY"] = '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'
 openai.api_key  = os.getenv("OPENAI_API_KEY")
 
 def get_completion(input, model="gpt-3.5-turbo"):
-    prompt = f"""
+    
+    type_prompt = f"""
     Determine the emergency type.
     energency types: FIRE CALLS, POLICE CALLS, EMS CALLS
+    Output the type only.
     ```{input}```
     """
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
+    etype = [{"role": "user", "content": type_prompt}]
+    etype_res = openai.ChatCompletion.create(
         model=model,
-        messages=messages,
+        messages=etype,
         temperature=0.1, # lower the 随机性 it is
     )
-    return response.choices[0].message["content"]
+    etype_res = etype_res.choices[0].message["content"]
+    print(etype_res)
 
-text = f"""
-    I'm in 100 College St.\
-"""
-location_prompt = f"""
+    location_prompt = f"""
     Determine the incident location from the message. 
     The output should be in the format "Apartment Number", "Street Number", 
     "Street Name". The output should be a dictionary.
     If any of the information is not available, put N/A as the value.
-```{text}```
+    ```{input}```
+    """
+    location = [{"role": "user", "content": location_prompt}]
+    location_res = openai.ChatCompletion.create(
+        model=model,
+        messages=location,
+        temperature=0.1, # lower the 随机性 it is
+    )
+    location_res = location_res.choices[0].message["content"]
+
+    name_prompt = f"""
+    Determine the name of the caller.
+    Output the name only.
+    ```{input}```
+    """
+    name = [{"role": "user", "content": name_prompt}]
+    name_res = openai.ChatCompletion.create(
+        model=model,
+        messages=name,
+        temperature=0.1, # lower the 随机性 it is
+    )
+    name_res = name_res.choices[0].message["content"]
+
+    sym = "NA"
+    if etype == 'EMS CALLS':
+        sym = None
+        sym_prompt = f"""
+            Determine the symptoms of the medical emergency.
+            Output in a list format of symptoms
+        ```{text}```
+        """
+        sym = [{"role": "user", "content": sym_prompt}]
+        sym_res = openai.ChatCompletion.create(
+            model=model,
+            messages=sym,
+            temperature=0.1, # lower the 随机性 it is
+        )
+        sym_res = sym_res.choices[0].message["content"]
+    cur_caller = Caller(caller_name = name_res, symptoms = sym, incident_type = etype_res, incident_location = location_res)
+    cur_caller.print_info()
+    return cur_caller
+    
+    #return response.choices[0].message["content"]
+
+text = f"""
+    My name is Amy. My friend fell down the stairs. Her ankle is swollen and 
+    her nose is bleeding. We are in 100 College St.\
 """
-location = get_completion(location_prompt)
+get_completion(text)
+# location_prompt = f"""
+#     Determine the incident location from the message. 
+#     The output should be in the format "Apartment Number", "Street Number", 
+#     "Street Name". The output should be a dictionary.
+#     If any of the information is not available, put N/A as the value.
+# ```{text}```
+# """
+# location = get_completion(location_prompt)
 
-type_prompt = f"""
-    Determine the emergency type.
-    energency types: FIRE CALLS, POLICE CALLS, EMS CALLS
-```{text}```
-"""
-etype = get_completion(location_prompt) 
+# type_prompt = f"""
+#     Determine the emergency type.
+#     energency types: FIRE CALLS, POLICE CALLS, EMS CALLS
+# ```{text}```
+# """
+# etype = get_completion(type_prompt) 
 
-type_prompt = f"""
-    Determine the emergency type.
-    energency types: FIRE CALLS, POLICE CALLS, EMS CALLS
-```{text}```
-"""
-etype = get_completion(location_prompt) 
+# name_prompt = f"""
+#     Determine the name of the caller.
+# ```{text}```
+# """
+# name = get_completion(name_prompt)
 
-
-
-print(location)
-
-
-
-# output
-# print(response) 
+# if etype == 'EMS CALLS':
+#     sym_prompt = f"""
+#         Determine the symptoms of the medical emergency.
+#         Output in a list format of symptoms
+#     ```{text}```
+#     """
+#     sym = get_completion(name_prompt) 
 
 
+# cur_caller = Caller(caller_name = name, symptoms = sym, incident_type = etype, incident_location = location)
+
+# print(cur_caller.print_info)
 '''
 emergency -> 
 type -> 
