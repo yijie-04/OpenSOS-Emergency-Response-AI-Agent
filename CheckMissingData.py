abfad64a735cfebe0ff8ff2a6aaf89c953ca67ff
RachelYangZY <ryangzy@gmail.com>
2023-11-04 21:54:49 -0400

    rachel

--- CheckMissingData.py
+++ CheckMissingData.py
@@ -3,11 +3,13 @@ from info import Caller
 def check(cur_caller):
     # do a for loop going through each required information. 
     # If None, have a prompt audibly ask for the information
-    if cur_caller.caller_name == None:
+    if cur_caller.caller_name == "XXX":
         return "What is your name?"
-    elif cur_caller.incident_location == None:
+    elif cur_caller.incident_location == "XXX":
         return "Where are you?"
-    elif cur_caller.symptoms == None:
+    elif cur_caller.incident_type == "XXX":
+        return "What is the emergency?"
+    elif cur_caller.symptoms == "XXX":
         return "What are the symptoms?"
     else:
         return None
--- InteractionWithCaller.py
+++ InteractionWithCaller.py
@@ -1,14 +1,14 @@
 import AudioToText
 import TextToAudio
 import API_1
-#from info import Caller
-#import CheckMissingData
+from info import Caller
+import CheckMissingData
 
 ## Call is received
 
 
 keep_talking = 1
-#cur_caller = Caller()
+cur_caller = Caller(caller_name = "XXX", symptoms = None, incident_type = None, incident_location = None)
 # initial prompt
 say = "9 1 1, what's your emergency?"
 while keep_talking == 1:
@@ -19,15 +19,14 @@ while keep_talking == 1:
     print(input)
 
     # send to AI Text Processing
-    classification = API_1.get_completion(input)
+    classification = API_1.get_completion(input, cur_caller)
     print(classification)
         # actually dont need classification here, it is needed for backend stuff
 
     # Determine if we need more information
-    #say = CheckMissingData.check(cur_caller)
-    #if say == None:
-    #    keep_talking = 0
+    say = CheckMissingData.check(cur_caller)
+    if say == None:
+        keep_talking = 0
 
-    keep_talking = 0
     
 
